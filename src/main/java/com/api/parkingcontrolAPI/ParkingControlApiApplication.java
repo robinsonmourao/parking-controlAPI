c6a01dc23df0eae5adf2b2c90432ba0f26f1af7a
robinsonmourao <49078615+robinsonmourao@users.noreply.github.com>
2022-09-30 08:51:18 -0300

    Initial commit

---
+++ src/main/java/com/api/parkingcontrolAPI/ParkingControlApiApplication.java
@@ -0,0 +1,14 @@
+package com.api.parkingcontrolAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApiApplication.class, args);
+	}
+
+}
+//https://www.youtube.com/watch?v=LXRU-Z36GEU&t=1904s
---
+++ src/test/java/com/api/parkingcontrolAPI/ParkingControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrolAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
